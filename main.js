9a2153f330d279c6d9703aaaadea4adf74b7c754
MarianeRemonte <mariane.remonte@gmail.com>
2026-06-24 10:32:06 -0300

    FINALMENTE faz o javascript funcionar e ajeita o código

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-// Seleciona todas as caixas
-const boxes = document.querySelectorAll(".fade-box");
+const elementos = document.querySelectorAll('div');
 
-// Função que ativa animação ao aparecer na tela
-function revealBoxes() {
-    const windowHeight = window.innerHeight;
+function revelar() {
+    elementos.forEach(elemento => {
+        const topo = elemento.getBoundingClientRect().top;
 
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top;
-
-        if (boxTop < windowHeight - 100) {
-            box.classList.add("show");
+        if (topo < window.innerHeight - 100) {
+            elemento.classList.add('show');
         }
     });
 }
 
+window.addEventListener('scroll', revelar);
+revelar();
+
+const imagem = document.getElementById('imagem-galeria');
+const btnProximo = document.getElementById('btn-proximo');
+const btnAnterior = document.getElementById('btn-anterior');
+ 
 const galeria = [
     "img/educacao.png",
-    "img/dificuldades.png",
-    "img/opiniao.png"
-]
-
-// Executa no scroll
-window.addEventListener("scroll", revealBoxes);
-
-// Executa ao carregar
-revealBoxes();
+    "dificuldades.png",
+    "opiniao.png"
+];
+ 
+let indiceAtual = 0;
+ 
+function atualizarGaleria() {
+    imagem.src = galeria[indiceAtual];
+}
+ 
+btnProximo.addEventListener('click', function() {
+    indiceAtual = (indiceAtual + 1) % galeria.length;
+    atualizarGaleria();
+});
+ 
+btnAnterior.addEventListener('click', function() {
+    indiceAtual = (indiceAtual - 1 + galeria.length) % galeria.length;
+    atualizarGaleria();
+});
+ 
+atualizarGaleria();
